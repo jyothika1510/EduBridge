b1f8ec452863287f0996656168db86999afccb3a
jyothika1510 <parachurijyothika.02@gmail.com>
2024-11-19 16:27:55 +0530

    hi

---
+++ AreaOfSquare.java
@@ -0,0 +1,13 @@
+class AreaOfSquare{
+     public static void findArea(){
+           int a = 5;
+        int area = a*a;
+        System.out.println("the area of :" + area);
+        
+     }
+     public static void main (String args[]){
+        System.out.println("finding the area");
+       
+        findArea();
+   }
+ }
